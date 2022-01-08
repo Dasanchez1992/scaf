596ee22e658286e26f22b5df5630ab13cd53e117
Danilo Sanchez <danilo.sanchez@nosolotec.com>
2022-01-08 13:02:19 -0500

    [FIX] Categoria, no obligatoria

--- scaf_sequence_products_by_serial/models/stock_production_lot.py
+++ scaf_sequence_products_by_serial/models/stock_production_lot.py
@@ -10,15 +10,16 @@ class StockProductionLot(models.Model):
         # Agregamos series categorias a prod
         obj = super(StockProductionLot, self).create(values)
         categ_id = obj.product_id.categ_id
-        if not categ_id.ir_sequence_id:
-            raise UserError('No hay sequencia creada para la categoria: {} del producto {}, sin la secuencia no se '
-                            'pueden crear automaticamente las secuencias de los numeros de serie de los '
-                            'productos'.format(
-                                categ_id.name,
-                                obj.product_id.name
-                            ))
-        obj.ref = "{}-{}".format(
-            categ_id.default_code,
-            categ_id.ir_sequence_id._next()
-        )
+        # if not categ_id.ir_sequence_id:
+        #     raise UserError('No hay sequencia creada para la categoria: {} del producto {}, sin la secuencia no se '
+        #                     'pueden crear automaticamente las secuencias de los numeros de serie de los '
+        #                     'productos'.format(
+        #                         categ_id.name,
+        #                         obj.product_id.name
+        #                     ))
+        if categ_id:
+            obj.ref = "{}-{}".format(
+                categ_id.default_code,
+                categ_id.ir_sequence_id._next()
+            )
         return obj
